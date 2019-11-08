d064eccba1c6965622030240ce85e8aab51058c2
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2019-11-08 11:03:42 -0500

    Minor testing for Outcome

--- src/tictactoe.ml
+++ src/tictactoe.ml
@@ -1,17 +1,24 @@
 
 (* Compile with ocamlbuild -use-ocamlfind tictactoe.native *)
 
+open Printf
+open Batteries
+
 open Board
 open Subboard
 open Outcome
 open Pos
 
-let fn (_, Pos (x, _)) = match x with
-    Left -> X
-  | Center -> Empty
-  | Right -> O
+module SubboardOutcome = Outcome.Make(Subboard)
+
+let fn (_, Pos (x, y)) = match x, y with
+    (Left, Top) | (Right, Bottom) -> X
+  | (Center, Middle) -> O
+  | _ -> Empty
 
 let () =
   let board = Board.init fn in
+  let subboard = Subboard.make board (Pos (Left, Top)) in
   print_endline (Board.to_string board);
+  printf "%c\n" (Pos.char_of_tic @@ SubboardOutcome.determine_winner subboard);
   print_endline "Hello! :)"
